de45d53595c395e6c961543f3e08074736b32270
Teddy <edwardcskurita@gmail.com>
2025-11-19 13:29:31 -0500

    changed file directory, connected to postgresql test database

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+
+public class Main {
+    public static void main() {
+        String url = "jdbc:postgresql://localhost:5432/final_proj_test";
+        String user = "postgres";
+        String password = "password";
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection connection = DriverManager.getConnection(url, user, password);
+            if (connection != null) {
+                System.out.println("Connected to the database");
+            } else  {
+                System.out.println("Failed to connect to the database");
+            }
+
+        } catch (Exception e){}
+    }
+}
